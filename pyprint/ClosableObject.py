2171ef8d1be62546e656543a13a7e7bde259799c
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-08-03 15:42:21 +0200

    Add ClosableObject
    This can be used for IO stuff. Avoid it where possible and try letting
    the objects user give you a handle or so so the handle needs to be
    closed and not whatever uses it.

---
+++ pyprint/ClosableObject.py
@@ -0,0 +1,46 @@
+def close_objects(*objs):
+    """
+    Determines for all given objects if an object is closable and closes
+    it if possible.
+
+    :param objs: The objects to close.
+    """
+    for obj in objs:
+        if isinstance(obj, ClosableObject):
+            obj.close()
+
+
+class ClosableObject:
+    """
+    Any class deriving from ClosableObject needs to be closed.
+    """
+
+    def __init__(self):
+        self._closed = False
+
+    def _close(self):
+        """
+        Override this method to implement own closing behaviour.
+        """
+        pass
+
+    def close(self):
+        """
+        Closes any IO Objects this object may hold.
+        """
+        if not self._closed:
+            self._close()
+            self._closed = True
+
+    def __del__(self):  # pragma: no cover
+        """
+        May warn if not closed properly - programmer error!
+
+        Obviously cannot deterministically be tested.
+        """
+        if not self._closed:
+            print("WARNING: {} was not closed. Cleaning up "
+                  "automatically. Note that this cleanup may not work "
+                  "on other interpreters and is nondeterministic."
+                  .format(repr(self)))
+            self.close()
---
+++ tests/ClosableObjectTest.py
@@ -0,0 +1,21 @@
+import unittest
+
+from pyprint.ClosableObject import ClosableObject, close_objects
+
+
+class ClosableObjectTest(unittest.TestCase):
+    def setUp(self):
+        self.uut = ClosableObject()
+
+    def test_closing(self):
+        self.assertFalse(self.uut._closed)
+        self.uut.close()
+        self.assertTrue(self.uut._closed)
+        self.uut.close()
+        self.assertTrue(self.uut._closed)
+
+    def test_close_objects(self):
+        close_objects(None, 5)
+        self.assertFalse(self.uut._closed)
+        close_objects(self.uut)
+        self.assertTrue(self.uut._closed)
