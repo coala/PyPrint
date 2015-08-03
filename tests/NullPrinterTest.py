9cf50288528ca7aea3b64001731d6100ffa57c04
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-08-03 16:03:47 +0200

    Add NullPrinter

---
+++ tests/NullPrinterTest.py
@@ -0,0 +1,10 @@
+import unittest
+
+from pyprint.NullPrinter import NullPrinter
+
+
+class NullPrinterTest(unittest.TestCase):
+    def test_non_printing(self):
+        self.uut = NullPrinter()
+        self.assertEqual(self.uut.print("anything"), None)
+        self.assertEqual(self.uut.print("anything", color="red"), None)
