ffbc215deca57d585d9f5809e1a330d81d2227af
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-08-03 15:56:24 +0200

    Add FilePrinter

---
+++ tests/FilePrinterTest.py
@@ -0,0 +1,23 @@
+from tempfile import TemporaryFile
+import unittest
+
+from pyprint.FilePrinter import FilePrinter
+
+
+class FilePrinterTest(unittest.TestCase):
+    def test_invalid_construction(self):
+        self.assertRaises(TypeError, FilePrinter, 5)
+
+    def test_printing(self):
+        self.file = TemporaryFile("w+")
+        self.uut = FilePrinter(self.file)
+
+        self.uut.print("Test value")
+
+        self.uut = FilePrinter(self.file)
+        self.uut.print("Test", "value2")
+
+        self.file.seek(0)
+        lines = self.file.readlines()
+
+        self.assertEqual(lines, ["Test value\n", "Test value2\n"])
