fa349bd405171ebe7954fd597bfa61a36a12684c
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-08-10 15:36:37 +0200

    Add ContextManagers

---
+++ pyprint/ContextManagers.py
@@ -0,0 +1,35 @@
+from contextlib import contextmanager, closing
+from io import StringIO
+import sys
+
+
+@contextmanager
+def replace_stdout(replacement):
+    """
+    Replaces stdout with the replacement, yields back to the caller and then
+    reverts everything back.
+
+    :param replacement: A file IO object to replace the stdout.
+    """
+    _stdout = sys.stdout
+    sys.stdout = replacement
+    try:
+        yield
+    finally:
+        sys.stdout = _stdout
+
+
+@contextmanager
+def retrieve_stdout():
+    """
+    Yields a StringIO object from which you can read everything that was
+    printed to stdout. (It won't be printed to the real stdout!)
+
+    Example usage:
+
+    with retrieve_stdout() as stdout:
+        print("something")  # Won't print to the console
+        what_was_printed = stdout.getvalue()  # Save the value
+    """
+    with closing(StringIO()) as sio, replace_stdout(sio):
+        yield sio
---
+++ tests/ContextManagersTest.py
@@ -0,0 +1,10 @@
+import unittest
+
+from pyprint.ContextManagers import retrieve_stdout
+
+
+class ContextManagersTest(unittest.TestCase):
+    def test_retrieve_stdout(self):  # Tests replace_stdout implicitly
+        with retrieve_stdout() as sio:
+            print("test")
+            self.assertEqual(sio.getvalue(), "test\n")
