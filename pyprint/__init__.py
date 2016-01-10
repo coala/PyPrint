d3604f401698c7b99f1a349214ec513cce56ff02
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-01-10 16:47:49 +0100

    pyprint: Add module summary
    Fixes https://github.com/coala-analyzer/PyPrint/issues/5

--- pyprint/__init__.py
+++ pyprint/__init__.py
@@ -1,1 +1,4 @@
-# TODO Package summary, provide objects here directly
+"""
+PyPrint provides Printer objects for Python so people can easily print stuff
+to different medias - in color - platform independently.
+"""
